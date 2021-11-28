924497fe843f0bbba7efa56ec195a5577ca3e3c8
mezhanglei <496623925@qq.com>
2021-11-28 22:36:29 +0800

    update

--- example/pages/home/index.tsx
+++ example/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect, useRef } from 'react';
 import "./index.less";
-import {Form, FormStore} from '../../../src/index';
+import { Form, FormStore } from '../../../src/index';
 
 
 class demo extends React.Component {
@@ -12,18 +12,26 @@ class demo extends React.Component {
     }
 
     onSubmit = async (e) => {
-        const { error, values } = await this.store.validate()
-        console.log(error, values, 'error ang values')
+        e.preventDefault();
+        const { error, values } = await this.store.validate();
+        console.log(error, values, 'error ang values');
     };
+
+    validator = (value, callError) => {
+        if (value?.length < 2) {
+            callError('主动报错');
+        }
+        callError();
+    }
 
     render() {
         return (
             <Form store={this.store} onSubmit={this.onSubmit}>
-                <Form.Field label="Name1" name="name1" rules={[{ required: true, message: 'Name1 is Empty' }]}>
-                  <input />
+                <Form.Field label="Name1" name="name1" rules={[{ required: true, message: 'Name1 is Empty' }, { validator: this.validator, message: '自定义校验' }]}>
+                    <input />
                 </Form.Field>
                 <Form.Field label="Name2" name="name2" rules={[{ required: true, message: 'Name2 is empty' }]}>
-                   <input />
+                    <input />
                 </Form.Field>
                 <Form.Field label="">
                     <button>Submit</button>
