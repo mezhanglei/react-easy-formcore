ec36bae9ae3684826519b190c2386d8029aeb1a9
mezhanglei <496623925@qq.com>
2023-06-27 12:05:53 +0800

    fix: subscrible error

--- lib/use-form.d.ts
+++ lib/use-form.d.ts
@@ -3,5 +3,5 @@ import { FormStore } from './form-store';
 import Validator from './validator';
 export declare function useFormStore<T extends Object = any>(values?: Partial<T>): FormStore<T>;
 export declare function useValidator(): Validator;
-export declare function useFormError(form: FormStore, path?: string): (import("react").Dispatch<import("react").SetStateAction<undefined>> | undefined)[];
-export declare function useFormValues<T = unknown>(form: FormStore, path?: string | string[]): T | undefined;
+export declare function useFormError(form: FormStore, path?: string, immediate?: boolean): (import("react").Dispatch<import("react").SetStateAction<undefined>> | undefined)[];
+export declare function useFormValues<T = unknown>(form: FormStore, path?: string | string[], immediate?: boolean): T | undefined;
